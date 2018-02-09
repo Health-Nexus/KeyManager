8f36975f7c8684d6158d55036a33cfed3f15b63f
Lucas Hendren <lhendre2@gmail.com>
2018-02-09 14:42:14 -0800

    fixed bug related to drs

--- src/app/drs/drs.component.ts
+++ src/app/drs/drs.component.ts
@@ -11,12 +11,13 @@ import { AsyncPipe } from '@angular/common';
 export class DrsComponent implements OnInit {
   @Input() tokenServices: any;
   @Input() drsServices: any;
-
-  constructor(private web3Service:Web3Service,private healthcashService:HealthcashService) { }
-  services:any= Array<{}>;
-  keys:any= Array<{}>;
+   services:any= [];
+   keys:any= [];
 
   urls: any={};
+  constructor(private web3Service:Web3Service,private healthcashService:HealthcashService) {
+   }
+
 
   ngOnInit() {
   }
@@ -24,22 +25,20 @@ export class DrsComponent implements OnInit {
   displayData(){
     this.services=this.web3Service.getServices()
     this.keys=this.web3Service.getKeys()
-    console.log('keys: 'this.keys)
+    console.log(this.services)
 
     for(var i=0;i<this.services.length;i++){
+      console.log('for loop',this.services[i]);
       this.urls[this.services[i]._service]=this.web3Service.getServiceURL(this.services[i]._service);
       this.services[i].url=this.web3Service.getServiceURL(this.services[i]._service);
 
     }
+    console.log(this.urls)
   }
 
   createService(url): any{
-   this.web3Service.createservice(url);
-  }
-
-  createKey(serviceId): any{
-    console.log('create service id: ',serviceId)
-    this.web3Service.createservice(serviceId);
+    console.log('here')
+    this.web3Service.createservice(url);
   }
 
   expand(service): any{
