c7aa31ac5983885d80995c3a1362a1fe9902da37
Kevin Cheli <kevincheli@Kevins-MacBook-Pro-2.local>
2019-06-13 23:53:02 -0400

    MetaMask web3 guard and install link

--- src/app/shared/window-ref.service.ts
+++ src/app/shared/window-ref.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@angular/core';
 
 function getWindow (): any {
+   
     return window;
+
 }
 
 @Injectable()
 export class WindowRefService {
+
     get nativeWindow (): any {
-        return getWindow();
+
+        if (window.hasOwnProperty('web3')){
+
+            return getWindow();
+
+        }
+
     }
 }
